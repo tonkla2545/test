ba4fef76f81af3085647364a79f26b9ab1ea2eb3
tonkla2545 <109588556+tonkla2545@users.noreply.github.com>
2023-05-08 14:31:36 +0700

    Update order.js

--- model/order.js
+++ model/order.js
@@ -4,6 +4,8 @@ const orderSchema = new mongoose.Schema({
     Pname: { type: String },
     Pprice: { type: Number },
     U_id: { type: mongoose.Schema.Types.ObjectId }
+    
+    
 });
 
 module.exports = mongoose.model('Order', orderSchema);
